1338b4f8ef278bac03c6d6c2095cfec773aacf1c
cmaulany <christian.maulany@kpn.com>
2022-06-02 16:05:41 +0200

    phase 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from 'react';
+import KeyValueInput from './components/KeyValueInput';
+
+const lookup = (variables, path) => {
+  if (typeof path === "string") {
+    path = path.split(".");
+  }
+
+  if (path.length === 0) {
+    return variables;
+  }
+
+  return lookup(variables[path[0]], path.slice(1));
+};
+
+const replace = (input, variables) => Object.entries(input).reduce(
+  (result, [key, value]) => {
+    if (value.startsWith("$")) {
+      result[key] = lookup(variables, value.slice(1));
+    } else {
+      result[key] = value;
+    }
+    return result;
+  },
+  {}
+);
 
 function App() {
+  const [variables, setVariables] = useState({
+    "california": "los_angeles",
+    "netherlands": "amsterdam"
+  });
+
+  const [input, setInput] = useState({
+    "location1": "los_angeles",
+    "location2": "$germany"
+  });
+
+  const result = useMemo(() => replace(input, variables), [variables, input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>Variables</h4>
+      <KeyValueInput value={variables} onChange={event => setVariables(event.target.value)} />
+      <h4>Input</h4>
+      <KeyValueInput value={input} onChange={event => setInput(event.target.value)} />
+      <h4>Result</h4>
+      <KeyValueInput value={result} onChange={event => setInput(event.target.value)} disabled={true} />
     </div>
   );
 }
